e084ecb09a53937e073de754d1e3e9a642fccba3
Jason_A_Fan_of_JJ_Lin <764901951@qq.com>
2023-05-06 21:37:25 +0800

    fix project 1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -39,15 +39,76 @@ public class ArrayDeque<T> {
     }
 
     public void addFirst(T item) {
-        if(size == length) {
+        if(isFull()) {
             grow();
-            array[size + 1] = item;
-        }else {
-            array[rear] = item;
-            rear = (rear + 1) % length;
         }
+        front = minusOne(front);
+        array[front] = item;
         size++;
     }
 
-    public void 
+    public void addLast(T item) {
+        if(isFull()) {
+            grow();
+        }
+
+        array[rear] = item;
+        rear = plusOne(rear, length);
+        size++;
+    }
+
+    public T removeFirst() {
+        if(isEmpty()) {
+            return null;
+        }
+
+        T result = array[front];
+        front = plusOne(front, length);
+        size--;
+        return result;
+    }
+
+    public T removeLast() {
+        if(isEmpty()) {
+            return null;
+        }
+
+        T result = array[rear];
+        rear = minusOne(rear);
+        size--;
+        return result;
+    }
+
+    private boolean isFull() {
+        return size == length;
+    }
+
+    private int minusOne(int index) {
+        return index == 0 ? length - 1 : index - 1;
+    }
+
+    private int plusOne(int index, int length) {
+        return index == length - 1 ? 0 : index + 1;
+    }
+
+    public T get(int index) {
+        if(index >= size) {
+            return null;
+        }
+
+        int ptr = front;
+        for(int i = 0; i < index; i++){
+            ptr = plusOne(ptr, length);
+        }
+        return array[ptr];
+    }
+
+    public void printDeque() {
+        int ptr = front;
+        while(ptr != rear) {
+            System.out.print(array[ptr] + " ");
+            ptr = plusOne(ptr, length);
+        }
+        System.out.println();
+    }
 }
