b9a3826b0697ffe71e7878e8bd3bfa3411641b5d
Jason_A_Fan_of_JJ_Lin <764901951@qq.com>
2023-05-23 14:59:02 +0800

    finish homework 1

--- hw1/GuitarHero.java
+++ hw1/GuitarHero.java
@@ -9,23 +9,23 @@ public class GuitarHero {
 
     private static final String KEYBOARD = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
 
-    private static final int totalNotes = 37;
+    private static final int TOTALNOTES = 37;
 
     private static double ithConcert(int i) {
         return CONCERT_A * Math.pow(2, (i - 24) / 12);
     }
 
     public static void main(String[] args) {
-        GuitarString[] guitarStrings = new GuitarString[totalNotes];
-        for (int i = 0; i < totalNotes; i++) {
+        GuitarString[] guitarStrings = new GuitarString[TOTALNOTES];
+        for (int i = 0; i < TOTALNOTES; i++) {
             guitarStrings[i] = new GuitarString(ithConcert(i));
         }
 
         while (true) {
-            if(StdDraw.hasNextKeyTyped()) {
+            if (StdDraw.hasNextKeyTyped()) {
                 char key = StdDraw.nextKeyTyped();
                 int index = KEYBOARD.indexOf(key);
-                if (index < 0){
+                if (index < 0) {
                     System.out.println("please enter valid key : " + KEYBOARD);
                     continue;
                 }
@@ -41,8 +41,5 @@ public class GuitarHero {
                 string.tic();
             }
         }
-
     }
-
-
 }
--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,12 +1,9 @@
-// TODO: Make sure to make this class a part of the synthesizer package
 package synthesizer;
 import java.util.Iterator;
 
 /**
  * @author 老爷保号
  */
-//TODO: Make sure to make this class and all of its methods public
-//TODO: Make sure to make this class extend AbstractBoundedQueue<t>
 public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
 
     /**
@@ -28,11 +25,6 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
      * Create a new ArrayRingBuffer with the given capacity.
      */
     public ArrayRingBuffer(int capacity) {
-        // TODO: Create new array with capacity elements.
-        //       first, last, and fillCount should all be set to 0.
-        //       this.capacity should be set appropriately. Note that the local variable
-        //       here shadows the field we inherit from AbstractBoundedQueue, so
-        //       you'll need to use this.capacity to set the capacity.
         rb = (T[]) new Object[capacity];
         first = last = fillCount = 0;
         this.capacity = capacity;
@@ -45,7 +37,6 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
      */
     @Override
     public void enqueue(T x) {
-        // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
         if(isFull()) {
             throw new RuntimeException("Ring buffer overflow");
         }
@@ -62,7 +53,6 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
      */
     @Override
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update
         if (isEmpty()){
             throw new RuntimeException("Ring buffer underflow");
         }
@@ -77,11 +67,12 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
      */
     @Override
     public T peek() {
-        // TODO: Return the first item. None of your instance variables should change.
+        if (fillCount == 0) {
+            throw new RuntimeException("Empty Ring buffer.");
+        }
         return rb[first];
     }
 
-    // TODO: When you get to part 5, implement the needed code to support iteration.
     /**
      * @author 老爷保号
      */
