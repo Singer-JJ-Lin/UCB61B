b6d946da648f99c384399a9d746d3c334f5f06a6
Jason_A_Fan_of_JJ_Lin <764901951@qq.com>
2023-06-15 11:41:44 +0800

    finish homework 4

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -174,10 +174,10 @@ public class Board implements WorldState {
     @Override
     public String toString() {
         StringBuilder s = new StringBuilder();
-        int N = size();
-        s.append(N + "\n");
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        int number = size();
+        s.append(number + "\n");
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j < number; j++) {
                 s.append(String.format("%2d ", tileAt(i, j)));
             }
             s.append("\n");
